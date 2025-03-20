553c0e5ed60d8f69865157fec16dae23309e5fd7
RahulBalhara <rahulbalhara821@gmail.com>
2025-03-20 16:14:43 +0530

    add features to videoCarousel.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,52 +1,52 @@
-import React  from 'react';
-import { useState ,useEffect} from "react";
+import React from 'react';
+import { useState, useEffect } from "react";
 
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import {heroVideo, smallHeroVideo} from "../utils";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
-    const [videoSrc,setVideoSrc]=useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
-    useGSAP(()=>{
-        gsap.to('#hero',{opacity:1,delay:2})
-        gsap.to("#cta",{opacity:1,y:-50 , delay:2}) 
-    },[])
-    
-    const handleVideoSrcSet = () => {
-      if(window.innerWidth < 760 ){
-        setVideoSrc(smallHeroVideo)
-      }
-      else{
-        setVideoSrc(heroVideo)
-      }
-    }
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay: 2 })
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 })
+  }, [])
 
-    useEffect( () =>{
-      window.addEventListener('resize',handleVideoSrcSet)
-      return ()=>{
-        window.removeEventListener('resize',handleVideoSrcSet)
-      }
-    },[])
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo)
+    }
+    else {
+      setVideoSrc(heroVideo)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet)
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet)
+    }
+  }, [])
 
   return (
-   <section className='w-full nav-height bg-black relative'>
-   <div className=" h-5/6 w-full flex-center flex-col">
-   <p  id="hero" className='hero-title'> iPhone 15 Pro</p>  
-   
-   <div className='md:w-9/12 w-9/12'>
-    <video className='pointer-events-none' autoPlay muted playsInline={true} key={videoSrc}> 
-      <source src={videoSrc} type='video/mp4'/>
-    </video>
-   </div>
-   </div>
-    <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
-    <a href="#highlights" className="btn">Buy </a>
-    <p className='fony-normal text-xl'>From $199/month or $999</p>
-    </div>
+    <section className='w-full nav-height bg-black relative'>
+      <div className=" h-5/6 w-full flex-center flex-col">
+        <p id="hero" className='hero-title'> iPhone 15 Pro</p>
+
+        <div className='md:w-9/12 w-9/12'>
+          <video className='pointer-events-none' autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type='video/mp4' />
+          </video>
+        </div>
+      </div>
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+        <a href="#highlights" className="btn">Buy </a>
+        <p className='fony-normal text-xl'>From $199/month or $999</p>
+      </div>
 
 
-   </section>
-    
+    </section>
+
   )
 }
 
